e280038dd2121b602e92caf725a1628a73a52130
Chris Webb <chris@chrisvwebb.com>
2013-12-08 21:37:25 -0500

    Send.php updates and index.html updates

--- send.php
+++ send.php
@@ -1,13 +1,30 @@
 <?php
   $subject = $_REQUEST['subject'] ;
   $message = $_REQUEST['photo'] ;
-  $message = $_REQUEST['message'] ;
+  $message = "[status pending]" . $_REQUEST['message'] ;
   mail("wole840foqo@post.wordpress.com", $subject,
   $message, "From:" . $email);
-  echo "Thank you for using our mail form";
-  
-  /* Redirect browser */
-header("Location: http://imd495.alexsablan.info");
-/* Make sure that code below does not get executed when we redirect. */
-exit;
+  $messageBody = "Thank you for using our mail form";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Send Email</title>
+<script src="jquery-1.6.4.min.js"></script>
+</head>
+<body onload="redirectForm();">
+<h2><?php echo($messageBody); ?></h2>
+<p><a href="http://imd495.alexsablan.info/projects/index.html">If not redirected click here.</a></p>
+<form id="thisForm" name="thisForm" action="http://imd495.alexsablan.info/projects/index.html" method="get">
+</form>
+<script>
+function redirectForm() {
+	document.getElementById("thisForm").submit();
+	window.location.assign("http://imd495.alexsablan.info/projects/index.html");
+}
+$(document).ready( function() {
+	$.mobile.changePage("http://imd495.alexsablan.info/projects/index.html");
+});
+</script>
+</body>
+</html>
